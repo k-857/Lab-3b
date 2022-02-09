9720f70b985950db576d6335ba3f22275c2e9d50
k-857 <60834543+k-857@users.noreply.github.com>
2022-02-08 20:23:04 -0500

    Add files via upload

---
+++ Js/res.js
@@ -0,0 +1,8 @@
+document.write("Investment amount = " + investment);
+document.write(" Interest rate = " + rate);
+document.write(" Years = " + years);
+document.write(" Future Value is " + futureValue + "<br>");
+
+document.getElementById("rate").style.color = red; // red or #ffffff
+document.getElementById("investment").style.color = red; // red or #ffffff
+document.getElementById("years").style.color = red; // red or #ffffff
