2989e2b9c977912afc09dace16f5cc140050047d
k-857 <60834543+k-857@users.noreply.github.com>
2022-02-19 12:38:05 -0500

    Delete Future Value directory

--- Future Value/Js/res.js
+++
@@ -1,4 +0,0 @@
-document.write("Investment amount = " + investment);
-document.write(" Interest rate = " + rate);
-document.write(" Years = " + years);
-document.write(" Future Value is " + futureValue + "<br>");
