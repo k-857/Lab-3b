b28a1ce19a2077de54a46a6f9791b7340ad006f8
k-857 <60834543+k-857@users.noreply.github.com>
2022-02-19 12:38:36 -0500

    Add files via upload

---
+++ res.js
@@ -0,0 +1,4 @@
+document.write("Investment amount = " + investment);
+document.write(" Interest rate = " + rate);
+document.write(" Years = " + years);
+document.write(" Future Value is " + futureValue + "<br>");
